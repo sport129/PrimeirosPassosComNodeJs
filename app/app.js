931a6777557f3521a86f60c731eb5c485fc580e9
Daniel Rosendo <daniel.rosendos@hotmail.com>
2018-12-05 17:08:34 -0300

    Termino cap 9

--- app/app.js
+++ app/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var expressNunjucks = require('express-nunjucks');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -11,7 +12,8 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'njk');
+njk = expressNunjucks(app);
 
 app.use(logger('dev'));
 app.use(express.json());
