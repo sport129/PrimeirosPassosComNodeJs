56fb2444acf2729c0e88af9f2256bf51c68444bd
Daniel Rosendo <daniel.rosendos@hotmail.com>
2018-12-06 16:35:11 -0300

    FinalozaçÃo do livro

--- app/app.js
+++ app/app.js
@@ -1,32 +1,60 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+//var favicon = require('serve-favicon');
 var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var passport = require('passport');
+var methodOverride = require('method-override');
+
+require('./passport');
+
 var expressNunjucks = require('express-nunjucks');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var register = require('./routes/register');
+var tasks = require('./routes/tasks');
 
 var app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'njk');
-njk = expressNunjucks(app);
+var njk = expressNunjucks(app,{watch:true, noCache: true});
+
+app.use(session({
+  secret: 'teste sessoes',
+  resave: false,
+  saveUninitialized: false,
+
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(methodOverride(function (req, res) {
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+
+    var method = req.body._method
+    delete req.body._method
+    return method
+  }
+}));
+
+app.use('/', register);
+app.use('/', tasks);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handler
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,15 +1,34 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+var isAuth = require('../middlewares/authorize.js').isAuth;
+var isNotAuth = require('../middlewares/authorize.js').isNotAuth;
 
-/* GET home page. */
-router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+router.get('/', isAuth, (req, res, next)=> {
+
+  res.render('home',{
+    session: req.session,
+    usuario: req.user
+  });
+
 });
 
-router.get('/tabuada/:valor', function(req, res, next){
-  var valor = req.params.valor;
-  var resultado = (valor % 2) == 0? valor : false;
-  res.render('tabuada', {numero : resultado});
+router.get('/login', isNotAuth, (req, res, next)=> {
+
+    res.render('login')
+
+});
+
+router.post('/', passport.authenticate('local',{
+  successRedirect: '/',
+  failureRedirect: '/error',
+
+}));
+
+
+router.get('/logout', (req, res, next)=> {
+  req.session.destroy();
+  res.redirect('/')
 });
 
 module.exports = router;
